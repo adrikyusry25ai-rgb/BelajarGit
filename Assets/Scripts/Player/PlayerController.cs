80ca2218e2cf212b0303f0a2534d9b7c25f093b1
adrikyusry25ai-rgb <adrikyusry.25.ai@gmail.com>
2026-05-11 17:09:40 +0700

    fix: connect player to scriptable object

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,21 +3,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float currentHP = 100;
-    public float speed = 5f;
+    [Header("Data")]
+    public PlayerData playerData;   
+
+    private float currentHP;
+    private float speed;
     private PlayerInput playerInput;
     private Vector2 moveInput;
 
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
+
+        currentHP = playerData.maxHP;
+        speed = playerData.moveSpeed;
     }
-    
-    
+
     void Update()
     {
         if (playerInput == null) return;
-        
+
         moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
         float h = moveInput.x;
         float v = moveInput.y;
